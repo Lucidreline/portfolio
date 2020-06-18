f927e60e047e608a849f13e1cddc934570275465
Manuel Castaneda <mcas@manuelc.me>
2020-06-18 14:13:54 -0700

    Removed material ui since I didn't end up using it

--- src/index.js
+++ src/index.js
@@ -3,28 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// material ui config
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#29b6f6',
-    },
-    secondary: {
-      main: '#ef5350',
-    },
-  },
-  typography: {
-    fontSize: 20,
-  },
-});
-
 ReactDOM.render(
   <React.StrictMode>
-    <MuiThemeProvider theme={theme}>
-      <App />
-    </MuiThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
