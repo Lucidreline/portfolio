6485afae6aeb04e6bd2c7cbbcfda81240ad26045
Lucidreline <techservs@pm.me>
2021-01-02 15:11:11 -0800

    Fixed the width of the sections. Changed an email address in the contact section.

--- src/components/sections/contact/contact.component.jsx
+++ src/components/sections/contact/contact.component.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-import './contact.styles.scss';
+import './contact.styles.scss'
 
 const Contact = () => {
-  return (
-    <section id='contact'>
-      <div className='content'>
-        <h2 className='section-title'>Let's get in touch?</h2>
-        <p className='marg-bottom'>
-          Have a problem that needs to be solved? Feel free to contact me
-          through my email{' '}
-          <a
-            className='clickable-color color-always'
-            href='mailto:mcas@manuelc.me'
-          >
-            mcas@manuelc.me
-          </a>
-          .
-        </p>
-        <p>I will respond as soon as I can.</p>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section id='contact'>
+			<div className='content'>
+				<h2 className='section-title'>Let's get in touch?</h2>
+				<p className='marg-bottom'>
+					Have a problem that needs to be solved? Feel free to contact me
+					through my email{' '}
+					<a
+						className='clickable-color color-always'
+						href='mailto:casta.ma502@gmail.com'
+					>
+						casta.ma502@gmail.com
+					</a>
+					.
+				</p>
+				<p>I will respond as soon as I can.</p>
+			</div>
+		</section>
+	)
+}
 
-export default Contact;
+export default Contact
