9bf861720e6d2eeef39574c9bbca813eb7b5bb9a
Lucidreline <techservs@pm.me>
2021-01-02 14:48:06 -0800

    Removed SahxShop. Changed URL to WhatsMyGrade. Changed contact email.

--- src/components/sections/projects/projects.data.js
+++ src/components/sections/projects/projects.data.js
@@ -11,18 +11,8 @@ const ProjectsData = {
         'A real-world website developed for the official Pre-Law Society student organization at CSUDH.',
     },
     {
-      name: 'SAHX Shop',
-      url: 'https://sahx.manuelc.me/',
-      github: 'https://github.com/Lucidreline/sahx-shop',
-      photo:
-        'https://res.cloudinary.com/dglxjd018/image/upload/v1600466198/Portfolio/screenshots/SAHX_xvlgdr.png',
-      technologies: 'React, Firebase',
-      description:
-        'A fictional online store that sells a variety of socks. You can browse around the socks by their category and even sign in using your google account.',
-    },
-    {
       name: "What's My Grade",
-      url: 'https://whatsmygrade.org/',
+      url: 'https://whatsmygrade.manuelc.me/',
       github: 'https://github.com/Lucidreline/whatsMyGrade',
       photo:
         'https://res.cloudinary.com/dglxjd018/image/upload/q_10/v1592364499/Portfolio/screenshots/whatsmygrade_mfxp7h.png',
--- src/components/side-links/side-links.component.jsx
+++ src/components/side-links/side-links.component.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
+import React from 'react'
 
-import './side-links.styles.scss';
+import './side-links.styles.scss'
 
 const SideLinks = () => {
-  return (
-    <div id='side-links'>
-      <ul>
-        <li>
-          <a
-            href='https://github.com/lucidreline'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='fab fa-github clickable-color color-hover'></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://www.linkedin.com/in/manuel-castaneda-bb588b17b/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='fab fa-linkedin clickable-color color-hover'></i>
-          </a>
-        </li>
-        <li>
-          <a href='mailto:mcas@manuelc.me'>
-            <i className='far fa-envelope  clickable-color color-hover'></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://drive.google.com/file/d/1mHZ8XSB8eyrHZQqqcOVfHyu0K72xHlEo/view?usp=sharing'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='far fa-file clickable-color color-hover'></i>
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-};
+	return (
+		<div id='side-links'>
+			<ul>
+				<li>
+					<a
+						href='https://github.com/lucidreline'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<i className='fab fa-github clickable-color color-hover'></i>
+					</a>
+				</li>
+				<li>
+					<a
+						href='https://www.linkedin.com/in/manuel-castaneda-bb588b17b/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<i className='fab fa-linkedin clickable-color color-hover'></i>
+					</a>
+				</li>
+				<li>
+					<a href='mailto:casta.ma502@gmail.com'>
+						<i className='far fa-envelope  clickable-color color-hover'></i>
+					</a>
+				</li>
+				<li>
+					<a
+						href='https://drive.google.com/file/d/1mHZ8XSB8eyrHZQqqcOVfHyu0K72xHlEo/view?usp=sharing'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<i className='far fa-file clickable-color color-hover'></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	)
+}
 
-export default SideLinks;
+export default SideLinks
