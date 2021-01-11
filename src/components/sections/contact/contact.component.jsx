bde764e8fe74a3ce864582231d9424d1448a973c
Lucidreline <casta.ma502@gmail.com>
2021-01-11 00:58:39 -0800

    Changed the accent color to red and added my discord name.

--- src/components/sections/contact/contact.component.jsx
+++ src/components/sections/contact/contact.component.jsx
@@ -8,17 +8,20 @@ const Contact = () => {
 			<div className='content'>
 				<h2 className='section-title'>Let's get in touch?</h2>
 				<p className='marg-bottom'>
-					Have a problem that needs to be solved? Feel free to contact me
-					through my email{' '}
+					Have a problem that needs to be solved? Just want to chat? Feel free
+					to contact me!
+				</p>
+				<p>
+					Email:{' '}
 					<a
 						className='clickable-color color-always'
 						href='mailto:casta.ma502@gmail.com'
 					>
 						casta.ma502@gmail.com
 					</a>
-					.
 				</p>
-				<p>I will respond as soon as I can.</p>
+				<p className='marg-bottom'>Discord: Lucidreline#2475</p>
+				<p>I will get back to you as soon as I can.</p>
 			</div>
 		</section>
 	)
