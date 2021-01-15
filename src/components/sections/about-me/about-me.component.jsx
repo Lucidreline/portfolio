ad0cbfcd1760a5fbf17b9f501c6eac118d3f2a5e
Lucidreline <casta.ma502@gmail.com>
2021-01-14 20:20:30 -0800

    Refined the Hero. Added icons to the contact section.

--- src/components/sections/about-me/about-me.component.jsx
+++ src/components/sections/about-me/about-me.component.jsx
@@ -1,102 +1,106 @@
-import React from 'react';
+import React from 'react'
 
-import './about-me.styles.scss';
+import './about-me.styles.scss'
 
 const AboutMe = () => {
-  return (
-    <section id='about-me'>
-      <div className='content'>
-        <h2 className='section-title'>About Me</h2>
-        <div className='content'>
-          <p className='marg-bottom'>
-            I wrote my first line of code right after I graduated high school in
-            2018 and immediately grew a passion for it. That lead me to become a
-            computer science major at{' '}
-            <a
-              className='clickable-color color-always'
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://www.csudh.edu/'
-            >
-              CSU Dominguez Hills
-            </a>
-            .
-          </p>
-          <p className='marg-bottom'>
-            In the last couple of months of 2018, I began my web development
-            journey. After seeing how I was able to build a beautiful user
-            interface for my apps, it became impossible for me to ever leave web
-            development.
-          </p>
-          <p className='marg-bottom'>
-            Here are some technologies I have been playing with recently:
-          </p>
-          <ul className='technologies'>
-            <li>
-              <a
-                className='clickable-color color-always'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://www.typescriptlang.org/'
-              >
-                TypeScript
-              </a>
-            </li>
-            <li>
-              <a
-                className='clickable-color color-always'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://reactjs.org/'
-              >
-                React
-              </a>
-            </li>
-            <li>
-              <a
-                className='clickable-color color-always'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://sass-lang.com/'
-              >
-                Sass
-              </a>
-            </li>
-            <li>
-              <a
-                className='clickable-color color-always'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://expressjs.com/'
-              >
-                Express
-              </a>
-            </li>
-            <li>
-              <a
-                className='clickable-color color-always'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://www.mongodb.com/'
-              >
-                MongoDB
-              </a>
-            </li>
-            <li>
-              <a
-                className='clickable-color color-always'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://git-scm.com/'
-              >
-                Git
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section id='about-me'>
+			<div className='content'>
+				<h2 className='section-title'>About Me</h2>
+				<div className='content'>
+					<p className='marg-bottom'>
+						I have a passion for developing and deploying full stack
+						applications for the web.
+					</p>
+					<p className='marg-bottom'>
+						I wrote my first line of code right after I graduated high school in
+						2018 and immediately grew a passion for it. That lead me to become a
+						computer science major at{' '}
+						<a
+							className='clickable-color color-always'
+							target='_blank'
+							rel='noopener noreferrer'
+							href='https://www.csudh.edu/'
+						>
+							CSU Dominguez Hills
+						</a>
+						.
+					</p>
+					<p className='marg-bottom'>
+						In the last couple of months of 2018, I began my web development
+						journey. After seeing how I was able to build a beautiful user
+						interface for my apps, it became impossible for me to ever leave web
+						development.
+					</p>
+					<p className='marg-bottom'>
+						Here are some technologies I have been playing with recently:
+					</p>
+					<ul className='technologies'>
+						<li>
+							<a
+								className='clickable-color color-always'
+								target='_blank'
+								rel='noopener noreferrer'
+								href='https://www.typescriptlang.org/'
+							>
+								TypeScript
+							</a>
+						</li>
+						<li>
+							<a
+								className='clickable-color color-always'
+								target='_blank'
+								rel='noopener noreferrer'
+								href='https://reactjs.org/'
+							>
+								React
+							</a>
+						</li>
+						<li>
+							<a
+								className='clickable-color color-always'
+								target='_blank'
+								rel='noopener noreferrer'
+								href='https://sass-lang.com/'
+							>
+								Sass
+							</a>
+						</li>
+						<li>
+							<a
+								className='clickable-color color-always'
+								target='_blank'
+								rel='noopener noreferrer'
+								href='https://expressjs.com/'
+							>
+								Express
+							</a>
+						</li>
+						<li>
+							<a
+								className='clickable-color color-always'
+								target='_blank'
+								rel='noopener noreferrer'
+								href='https://www.mongodb.com/'
+							>
+								MongoDB
+							</a>
+						</li>
+						<li>
+							<a
+								className='clickable-color color-always'
+								target='_blank'
+								rel='noopener noreferrer'
+								href='https://git-scm.com/'
+							>
+								Git
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default AboutMe;
+export default AboutMe
--- src/components/sections/contact/contact.component.jsx
+++ src/components/sections/contact/contact.component.jsx
@@ -6,21 +6,23 @@ const Contact = () => {
 	return (
 		<section id='contact'>
 			<div className='content'>
-				<h2 className='section-title'>Let's get in touch?</h2>
+				<h2 className='section-title'>Get in touch with me</h2>
 				<p className='marg-bottom'>
 					Have a problem that needs to be solved? Just want to chat? Feel free
 					to contact me!
 				</p>
 				<p>
-					Email:{' '}
+					<i className='contact-icons far fa-envelope'></i>
 					<a
-						className='clickable-color color-always'
+						className='clickable-color color-hover'
 						href='mailto:casta.ma502@gmail.com'
 					>
-						casta.ma502@gmail.com
+						Casta.ma502@gmail.com
 					</a>
 				</p>
-				<p className='marg-bottom'>Discord: Lucidreline#2475</p>
+				<p className='marg-bottom'>
+					<i className='fab fa-discord contact-icons'></i>Lucidreline#2475
+				</p>
 				<p>I will get back to you as soon as I can.</p>
 			</div>
 		</section>
