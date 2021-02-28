fab259b6582e2b601913438baa37cd7a22ef7c43
Lucidreline <casta.ma502@gmail.com>
2021-02-27 23:18:04 -0800

    Added a prettier config, removed elevatorBtn from app.js, and added react router dom.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-import './App.scss';
+import './App.scss'
 
 // components
-import Header from './components/header/header.component';
-import Hero from './components/sections/hero/hero.component';
-import AboutMe from './components/sections/about-me/about-me.component';
-import Projects from './components/sections/projects/projects.component';
-import Contact from './components/sections/contact/contact.component';
-import ElevatorBtn from './components/elevator-btn/elevator-btn.component';
+import Header from './components/header/header.component'
+import Hero from './components/sections/hero/hero.component'
+import AboutMe from './components/sections/about-me/about-me.component'
+import Projects from './components/sections/projects/projects.component'
+import Contact from './components/sections/contact/contact.component'
 
 // animations
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-AOS.init();
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { Switch, Route } from 'react-router-dom'
+AOS.init()
 
 function App() {
   return (
-    <div className='App'>
-      <div id='margin-container'>
+    <div className="App">
+      <div id="margin-container">
+      <div data-aos="fade-right">
         <Header />
-        <div data-aos='fade-right'>
-          <Hero />
+      </div>
+        <div data-aos="fade-up-right">
+        <Switch>
+          <Route exact to="/" component={Hero}/>
+          <Route to="/" component={Projects}/>
+          <Route to="/" component={AboutMe}/>
+          <Route to="/" component={Contact}/>
+        </Switch>
         </div>
-        <div data-aos='fade-up-right'>
-          <Projects />
-        </div>
-        <div data-aos='fade-up-right'>
-          <AboutMe />
-        </div>
-        <div data-aos='fade-up-right'>
-          <Contact />
-        </div>
-        <div data-aos='flip-up'>
-          <ElevatorBtn />
-        </div>
+        
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
